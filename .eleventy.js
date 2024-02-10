a9b25a3aad9340ca4c9dc1083fbbc284cc1e19ea
Christian Glahn <cpglahn@gmail.com>
2024-02-10 22:30:51 +0000

    add date Filters for posts

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,24 @@
 const yaml = require("yaml");
+const { DateTime } = require("luxon");
 
 module.exports = config => {
     config.addPassthroughCopy('./assets/');
     config.addPassthroughCopy("./images/");
 
+    
     config.addDataExtension("yaml", contents => yaml.parse(contents));
+
+    config.addFilter('htmlDateString', (dateObj) => {
+		// dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+		return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+	});
+
+    config.addFilter('readableDate', (dateObj) => {
+		// dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+		return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('dd LLLL yyyy');
+	});
+
+    config.addFilter("entryLimit", (arr, limit) => arr.slice(0, limit));
 
     return {
       markdownTemplateEngine: 'njk',
