fa87fa4d014bbd6f3f2c66254698f390766486e7
Christian Glahn <cpglahn@gmail.com>
2024-02-13 13:49:13 +0000

    handle images and dates correctly

--- .eleventy.js
+++ .eleventy.js
@@ -2,20 +2,35 @@ const yaml = require("yaml");
 const { DateTime } = require("luxon");
 
 module.exports = config => {
+
+    // the following line is required for stylesheet development
+    config.setServerPassthroughCopyBehavior("passthrough");
+
     config.addPassthroughCopy('./assets/');
     config.addPassthroughCopy("./images/");
-
+    config.addPassthroughCopy("**/*.jpg");
+    config.addPassthroughCopy("**/*.png");
+    config.addPassthroughCopy("**/*.svg");
+    config.addPassthroughCopy("**/*.pdf");
     
     config.addDataExtension("yaml", contents => yaml.parse(contents));
 
+    config.addShortcode("thisYear", () => DateTime.now().setZone('Europe/Amsterdam').toFormat('yyyy'));
+    config.addShortcode("thisDate", () => DateTime.now().setZone('Europe/Amsterdam').toFormat('yyyy-LL-dd'));
+
+    config.addFilter('dateYear', (dateObj) => {
+		// dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+		return `${DateTime.fromJSDate(dateObj, {zone: 'Europe/Amsterdam'}).toFormat('yyyy')}`;
+	});
+
     config.addFilter('htmlDateString', (dateObj) => {
 		// dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-		return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+		return `${DateTime.fromJSDate(dateObj, {zone: 'Europe/Amsterdam'}).toFormat('yyyy-LL-dd')}`;
 	});
 
     config.addFilter('readableDate', (dateObj) => {
 		// dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-		return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('dd LLLL yyyy');
+		return DateTime.fromJSDate(dateObj, {zone: 'Europe/Amsterdam'}).toFormat('dd LLLL yyyy');
 	});
 
     config.addFilter("entryLimit", (arr, limit) => arr.slice(0, limit));
